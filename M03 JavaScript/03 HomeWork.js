cc8505d7403eb9be899c76ec4e4c769d4d1defc0
Oliverotalorap <oliverotalorapor10@gmail.com>
2024-06-06 12:03:00 -0500

    240606 Jue commit 2023

--- M03 JavaScript/03 HomeWork.js
+++ M03 JavaScript/03 HomeWork.js
@@ -33,8 +33,8 @@ function combinarNombres(nombre, apellido) {
     // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
     // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
     // Tu código:
-    console.log(nombre,  apellido);
     return nombre + " " + apellido;
+    console.log(nombre,  apellido);
     
  }
 console.log(combinarNombres("Oliver", "Porras"));
